7cd31b0f52a1a6ef50605b07f51d983f8d6a2cda
TeoSulaymonov95 <turobsulaymonov9@gmail.com>
2024-08-10 20:16:08 +0900

    feat:validation  develop login and signup page

--- src/components/auth/register.tsx
+++ src/components/auth/register.tsx
@@ -1,10 +1,31 @@
+import { registerSchema } from '@/lib/validation'
 import { useAuthState } from '@/stores/auth.store'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
 import { Button } from '../ui/button'
+import {
+	Form,
+	FormControl,
+	FormField,
+	FormItem,
+	FormLabel,
+	FormMessage,
+} from '../ui/form'
 import { Input } from '../ui/input'
 import { Separator } from '../ui/separator'
 
 const Register = () => {
 	const { setAuth } = useAuthState()
+
+	const form = useForm<z.infer<typeof registerSchema>>({
+		resolver: zodResolver(registerSchema),
+		defaultValues: { email: '', passowrd: '' },
+	})
+
+	const onSubmit = async (values: z.infer<typeof registerSchema>) => {
+		const { email, passowrd } = values
+	}
 
 	return (
 		<div className='flex flex-col'>
@@ -19,21 +40,56 @@ const Register = () => {
 				</span>
 			</p>
 			<Separator className='my-3' />
-			<div>
-				<span>Email</span>
-				<Input placeholder='example@gmail.com' />
-			</div>
-			<div className='grid grid-cols-2 gap-4 mt-2'>
-				<div>
-					<span>Password</span>
-					<Input placeholder='*****' type='password' />
-				</div>
-				<div>
-					<span>Confirm password</span>
-					<Input placeholder='*****' type='password' />
-				</div>
-			</div>
-			<Button className='w-full h-12 mt-2'>Register</Button>
+			<Form {...form}>
+				<form onSubmit={form.handleSubmit(onSubmit)} className='space-y-2'>
+					<FormField
+						control={form.control}
+						name='email'
+						render={({ field }) => (
+							<FormItem>
+								<FormLabel>Email address</FormLabel>
+								<FormControl>
+									<Input placeholder='example@gmail.com' {...field} />
+								</FormControl>
+								<FormMessage />
+							</FormItem>
+						)}
+					/>
+					<div className='grid grid-cols-2 gap-2'>
+						<FormField
+							control={form.control}
+							name='passowrd'
+							render={({ field }) => (
+								<FormItem>
+									<FormLabel>Password</FormLabel>
+									<FormControl>
+										<Input placeholder='*****' type='password' {...field} />
+									</FormControl>
+									<FormMessage />
+								</FormItem>
+							)}
+						/>
+						<FormField
+							control={form.control}
+							name='confirmPassword'
+							render={({ field }) => (
+								<FormItem>
+									<FormLabel>Confirm Password</FormLabel>
+									<FormControl>
+										<Input placeholder='*****' type='password' {...field} />
+									</FormControl>
+									<FormMessage />
+								</FormItem>
+							)}
+						/>
+					</div>
+					<div>
+						<Button type='submit' className='h-12 w-full mt-2'>
+							Submit
+						</Button>
+					</div>
+				</form>
+			</Form>
 		</div>
 	)
 }
---
+++ src/lib/validation.ts
@@ -0,0 +1,17 @@
+import { z } from 'zod'
+
+export const loginSchema = z.object({
+	email: z.string().email(),
+	passowrd: z.string().min(8),
+})
+
+export const registerSchema = z
+	.object({
+		email: z.string().email(),
+		passowrd: z.string().min(8),
+		confirmPassword: z.string(),
+	})
+	.refine(data => data.passowrd === data.confirmPassword, {
+		message: 'Password do not match',
+		path: ['confirmPassword'],
+	})
