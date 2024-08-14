2b7538f53dd9794a8e7a1a1a0862a5af891da07b
TeoSulaymonov95 <turobsulaymonov9@gmail.com>
2024-08-14 20:51:32 +0900

    feat: update and delete business logic

--- src/main.tsx
+++ src/main.tsx
@@ -5,15 +5,21 @@ import './styles/index.css'
 import { ThemeProvider } from './components/providers/theme-provider.tsx'
 import { BrowserRouter } from 'react-router-dom'
 import AuthProvider from './components/providers/auth-provider.tsx'
+import {QueryClient,
+     QueryClientProvider
+} from '@tanstack/react-query'
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
+    <QueryClientProvider client={queryClient}>
     <ThemeProvider defaultTheme='dark'>
       <AuthProvider>
       <App />
       </AuthProvider>
      </ThemeProvider>
+    </QueryClientProvider>
     </BrowserRouter>
      </React.StrictMode>,
 )
